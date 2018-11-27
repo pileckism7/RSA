fcc2cf70d7cd4a161a69c4c3a5571613cb4823e8
pileckism7 <pileckism7@github.com>
2018-11-26 19:19:05 -0500

    Initial Classes with Emtpy methods

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,36 @@
-
+/**
+ * 
+ * @author Kieran Walsh, Martin Price, Mantas Pileckis
+ *
+ */
 public class Person {
-
+	long exponent;
+	long modulus;
+	
+	public Person(){
+		//TODO
+		//Generate a public key for this person, consisting of exponent,e, and modulus, m.
+		//Generate a private key, consisting of an exponent, d.
+		//Provide access to the public key only.
+		
+	}
+	// Decrypt the cipher text
+	public String decrypt(long[] cipher) {
+		return null;
+	}
+	
+	// Encrypt a plain text message to she.
+	public  long[]	encryptTo(java.lang.String msg, Person she) {
+		return null;
+	}
+	
+	//Access the public encryption exponent
+	public  long getE() {
+		return exponent;
+	}
+	
+	//Access the public modulus
+	public long getM(){
+		return modulus;
+	}
 }
--- src/RSA.java
+++ src/RSA.java
@@ -1,4 +1,44 @@
-
+/**
+ * 
+ * @author Kieran Walsh, Martin Price, Mantas Pileckis
+ *
+ */
 public class RSA {
+
+	public RSA(){
+	}
+
+	static void	main(java.lang.String[] args){
+
+	}
+	//Find the multiplicative inverse of a long int, mod m
+	public static long inverse(long e, long m)
+	{
+		return null;
+	}
+	//Convert a long to 2 chars
+	public static String longTo2Chars(long x){
+		return null;
+	}
+	//Raise a number, b, to a power, p, modulo m
+	public static long	modPower(long b, long p, long m){
+		return null;
+	}
+	// Find a randomg prime number
+	public static long	randPrime(int m, int n, java.util.Random rand) {
+		return null;
+	}
+	//Find a random number relatively prime to a given long int
+	public static long	relPrime(long n, java.util.Random rand) {
+		return null;
+	}
+	//Display an array of longs on stdout
+	public static void	show(long[] cipher) {
+	}
+	//Convert two numeric chars to a long
+	public static long	toLong(java.lang.String msg, int p) {
+		return null;
+	}
+
 
 }
