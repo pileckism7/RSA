4dc18e3e47a089b6e1e0015a81e0b8dbeb6d37de
pileckism7 <pileckism7@github.com>
2018-11-26 19:31:01 -0500

    Updated main

--- src/RSA.java
+++ src/RSA.java
@@ -8,8 +8,31 @@ public class RSA {
 	public RSA(){
 	}
 
-	static void	main(java.lang.String[] args){
+	public static void main (String args[])
+	{ 	
+		Person Alice = new Person();
+		Person Bob = new Person();
 
+		String msg = new String ("Bob, let's have lunch."); 	// message to be sent to Bob
+		long []  cipher;
+		cipher =  Alice.encryptTo(msg, Bob);			// encrypted, with Bob's public key
+
+		System.out.println ("Message is: " + msg);
+		System.out.println ("Alice sends:");
+		show (cipher);
+
+		System.out.println ("Bob decodes and reads: " + Bob.decrypt (cipher));	// decrypted,
+								// with Bob's private key.
+		System.out.println ();
+
+		msg = new String ("No thanks, I'm busy");
+		cipher = Bob.encryptTo (msg, Alice);
+	
+		System.out.println ("Message is: " + msg);
+		System.out.println ("Bob sends:");
+		show (cipher);
+
+		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
 	}
 	//Find the multiplicative inverse of a long int, mod m
 	public static long inverse(long e, long m)
