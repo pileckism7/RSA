789267fd77d8c7740a14e7b0b69ecbb4825b91a3
Mantas Pileckis <pileckism7@students.rowan.edu>
2018-12-02 17:35:44 -0500

    Inverse and modPower
    Done, Tested methods.

--- src/RSA.java
+++ src/RSA.java
@@ -8,7 +8,7 @@ public class RSA {
 	public RSA(){
 	}
 
-	public static void main (String args[])
+	/**public static void main (String args[])
 	{ 	
 		Person Alice = new Person();
 		Person Bob = new Person();
@@ -22,32 +22,81 @@ public class RSA {
 		show (cipher);
 
 		System.out.println ("Bob decodes and reads: " + Bob.decrypt (cipher));	// decrypted,
-								// with Bob's private key.
+		// with Bob's private key.
 		System.out.println ();
 
 		msg = new String ("No thanks, I'm busy");
 		cipher = Bob.encryptTo (msg, Alice);
-	
+
 		System.out.println ("Message is: " + msg);
 		System.out.println ("Bob sends:");
 		show (cipher);
 
 		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
+	}**/
+	public static void main (String args[]) {
+		// Test for inverse, Works good. // 13inverse mod 60 == 37.
+		System.out.println(inverse(13,60));
+		
+		// Test for Mod power, Works good.  // 17^13 mod 77 == 73.
+		System.out.println(modPower(17,13,77));
 	}
-	//Find the multiplicative inverse of a long int, mod m
-	public static long inverse(long e, long m)
-	{
-		return null;
-	}
+	
+	/**@author Mantas Pileckis
+	 * 	Find the multiplicative inverse of a long e, mod m
+	 * @param e Number
+	 * @param m Modulus
+	 * @return Result of inverse Number modded by m
+	 */
+
+	public static long inverse(long e, long m) { 
+        long m0 = m; 
+        long y = 0;
+        long x = 1; 
+  
+        if (m == 1) {
+            return 0;
+        }
+        while (e > 1) { 
+            long q = e / m; 
+            long t = m; 
+            m = e % m; 
+            e = t; 
+            t = y; 
+            y = x - q * y; 
+            x = t; 
+        } 
+        if (x < 0) {
+            x += m0;
+        }
+  
+        return x; 
+    } 
 	//Convert a long to 2 chars
 	public static String longTo2Chars(long x){
 		return null;
 	}
-	//Raise a number, b, to a power, p, modulo m
-	public static long	modPower(long b, long p, long m){
-		return null;
+	/**@author Mantas Pileckis
+	 * Raise a number, b, to a power, p, modulo m
+	 * @param b Number
+	 * @param p Power 
+	 * @param m Modulus
+	 * @return Result of (number raised to power) mod
+	 *
+	 */
+	public static long	modPower(long b, long p, long m) {
+		long result = 1;      
+		b = b % m;  
+		while (p > 0) { 
+			if((p & 1)==1) {
+				result = (result * b) % m;
+			}
+			p = p >> 1;  
+			b = (b * b) % m;  
+		} 
+		return result; 
 	}
-	// Find a randomg prime number
+	// Find a random prime number
 	public static long	randPrime(int m, int n, java.util.Random rand) {
 		return null;
 	}
